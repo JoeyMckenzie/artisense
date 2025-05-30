b954cdaa06f83531b59186f7e441a3f64a292175
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-05-30 12:33:50 -0700

    chore: config exception clean up

--- src/Exceptions/ArtisenseConfigurationException.php
+++ src/Exceptions/ArtisenseConfigurationException.php
@@ -15,36 +15,6 @@ final class ArtisenseConfigurationException extends Exception
 
     public static function invalidConfiguration(string $message): self
     {
-        return new self("Configuration is invalid: $message.");
-    }
-
-    public static function invalidVersion(): self
-    {
-        return new self("Documentation version must be a valid version string (e.g., '12.x', '11.x', 'master', etc.).");
-    }
-
-    public static function invalidVersions(): self
-    {
-        return new self("Documentation versions must be a valid version strings (e.g., '12.x', '11.x', 'master', etc.).");
-    }
-
-    public static function missingVersion(): self
-    {
-        return new self('Documentation version must be configured in your config file.');
-    }
-
-    public static function invalidPreference(): self
-    {
-        return new self('Invalid search preference. Must be either "ordered" or "unordered".');
-    }
-
-    public static function missingPreference(): self
-    {
-        return new self('Search preference must be configured in your config file.');
-    }
-
-    public static function invalidProximity(): self
-    {
-        return new self('Search proximity must be a positive integer.');
+        return new self("Configuration is invalid: $message");
     }
 }
